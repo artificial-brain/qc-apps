0ea2fb9ea6751f0b0c7beae2dd7883d906e3e749
ab-jiteshlalwani <jitesh.lalwani@artificialbrain.us>
2021-07-16 11:17:51 +0530

    Added protein folding demo

--- qc-apps/apps/android/QuantumWheel/app/src/main/java/com/artificialbrain/quantumwheel/models/QuantumRandomNumber.java
+++
@@ -1,9 +0,0 @@
-package com.artificialbrain.quantumwheel.models;
-
-public class QuantumRandomNumber {
-    public String getQuantum_random_num() {
-        return quantum_random_num;
-    }
-
-    private String quantum_random_num;
-}
